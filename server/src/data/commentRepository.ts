5ec07770ff973b39f9a8e5b812e1c81ca5e7e604
nick-funk <nick.funk@outlook.com>
2021-07-13 23:46:03 -0600

    Seed comments with one initial comment

--- server/src/data/commentRepository.ts
+++ server/src/data/commentRepository.ts
@@ -11,7 +11,13 @@ export class CommentRepository {
     this.comments = [];
   }
 
-  public seed() {}
+  public seed() {
+    this.comments.push({
+      id: "0632a850-5187-4829-b852-7a6dc453af7e",
+      authorID: "a1f237b0-df23-4cb4-8439-e82b75d627b1",
+      body: "Hello, everybody!",
+    });
+  }
 
   public findById(id: string) {
     return this.comments.find((i) => i.id === id);
